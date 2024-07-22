65aad9519cbaabc1228aea3f59ded99065e44f79
alejab2302@gmail.com <alejab2302@gmail.com>
2024-07-22 09:00:47 -0500

    Changes to be committed: 	modified:   index.html 	new file:   netlify.toml 	new file:   netlify/functions/server.js 	modified:   package.json 	renamed:    src/data/propiedades.json -> propiedades.json 	modified:   public/css/estilos.css 	new file:   server.js 	modified:   src/paginas/Property.jsx  implementacion json server

---
+++ netlify/functions/server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use('/.netlify/functions/server', router);
+
+module.exports.handler = server.listen(3000).handler;
--- src/paginas/Property.jsx
+++ src/paginas/Property.jsx
@@ -1,17 +1,67 @@
 import Navegacion from "../layout/Navegacion"
 import HeroGeneral from "../layout/HeroGeneral"
 import FooterGeneral from "../layout/FooterGeneral"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Button } from "flowbite-react";
+import { HiOutlineArrowLeft } from "react-icons/hi";
+
 
 const Property = () => {
+
+  const { id } = useParams();
+
+  const [propiedad, setPropiedad] = useState([]);
+
+  useEffect(() => {
+    const obtenerPropiedad = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:8000/propiedades");
+        setPropiedad(data[id - 1]);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    obtenerPropiedad();
+  }, []);
+
+  const { titulo, ciudad, descripcion, mapa, imagenes } = propiedad;
+
+
   return (
 
     <>
-      <Navegacion/>
+      <Navegacion />
       <HeroGeneral
-        pagina="Property"
+        pagina="Properties"
       />
-      <div>Property</div>
-      <FooterGeneral/>
+
+      <section className="pt-10 pb-5">
+        <div className="w-11/12 m-auto">
+
+          <Button outline pill>
+            <HiOutlineArrowLeft className="h-4 w-4 " />
+          </Button>
+
+        </div>
+        <div className="w-10/12 m-auto flex justify-between font-bold text-xs lg:text-2xl">
+
+
+          <p className="text-cyan-950 pl-2">{titulo}</p>
+
+
+          <p className="text-yellow-600">{ciudad}</p>
+        </div>
+
+      </section >
+      <section className="pb-10">
+        <div className="w-10/12 m-auto " >
+          <iframe className="w-full h-60" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d18557.364409156176!2d-122.21657593583618!3d40.18698711810877!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2smx!4v1721612581591!5m2!1ses!2smx" style={{ border: "0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
+      </section>
+      <FooterGeneral />
     </>
   )
 }
