37bba758fb469c7d85561adb56107e71fb40d18f
alejab2302@gmail.com <alejab2302@gmail.com>
2024-07-22 09:22:30 -0500

    On branch main

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 
-
 const jsonServer = require('json-server');
+const path = require('path');
+const express = require('express');
+const app = express();
 const server = jsonServer.create();
-const router = jsonServer.router('propiedades.json'); // Archivo JSON con datos
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 8000;
 
-server.use(middlewares);
-server.use(router);
+const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+// Middleware para servir archivos estáticos de la carpeta build
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(middlewares);
+app.use('/api', router);
+
+// Servir el index.html para cualquier ruta que no sea API
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
