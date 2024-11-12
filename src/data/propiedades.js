a8e1582902599906e574dd745939bcf85f3a3185
MariaBarros02 <141078532+MariaBarros02@users.noreply.github.com>
2024-11-12 14:52:11 -0500

    Actualizacion propiedad 3

--- src/data/propiedades.js
+++ src/data/propiedades.js
@@ -8,9 +8,9 @@ const propiedades = [
         descripcion: "Relax in this tranquil and elegant space. This fully equipped house offers comfortable sleeping areas, with curtains in all bedrooms for a better rest. Enjoy a well-equipped kitchen, free parking, and a location just 5 minutes from downtown and the Red Bluff fairgrounds!",
         mapa: "pb=!1m13!1m8!1m3!1d13042.515609502474!2d-122.23470694268649!3d40.1837448036871!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDExJzE2LjkiTiAxMjLCsDEzJzAzLjIiVw!5e0!3m2!1ses!2sco!4v1721677156090!5m2!1ses!2sco",
         imagenes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-        puntuacion: 4.88,
-        calificaciones: 25,
-        estrellas:[90,5,5,0,0],
+        puntuacion: 4.85,
+        calificaciones: 45,
+        estrellas: [90, 5, 5, 0, 0],
         link: "https://www.airbnb.mx/rooms/1043126792881083141?guests=1&adults=1&s=67&unique_share_id=57a6f5cb-3d25-42b8-a015-91ae1e1d472b",
         invitados: 9,
         entretenimiento: "For your entertainment, the property offers a TV, exercise equipment, and a selection of books and reading material, ensuring there’s something for everyone to enjoy during their stay.",
@@ -23,14 +23,32 @@ const propiedades = [
         parrafo: "11 Guests | 4 Bedrooms | 6 Beds | 2 Bathrooms ",
         descripcion: " The house is new, has a large new kitchen, stove/oven, dishwasher and much more. Located just 4 minutes from downtown Corning, close to Starbucks, Safe Way, Taco Bell and a variety of restaurants, just 9 minutes from Rolling Hills Casino. It has enough parking for trailers.",
         mapa: "pb=!1m18!1m12!1m3!1d48954.88733910236!2d-122.22332732500438!3d39.926166582357475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8082f29c136ae2f5%3A0x103342cc7d25c3f3!2sCorning%2C%20California%2096021%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sco!4v1722559386569!5m2!1ses-419!2sco",
-        imagenes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48],
+        imagenes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
         puntuacion: 4.85,
-        calificaciones: 34,
-        estrellas:[86,10,4,0,0],
+        calificaciones: 41,
+        estrellas: [86, 10, 4, 0, 0],
         link: "https://www.airbnb.mx/rooms/566777272277410256?viralityEntryPoint=1&s=76",
         invitados: 11,
         entretenimiento: "For your entertainment, the property offers a TV, high quality Wifi and the large private garden is a true treasure for families with children and babies.",
         areaFamiliar: "The house has a large dining table, it offers everything you need to enjoy a home-cooked meal together and outdoor furniture, it is perfect for enjoying the sunny weather while the little ones play safely, you can also work comfortably in the work area located in the living room. Private parking provides convenience and security, with more than enough space for several vehicles."
+
+
+    },
+    {
+        id: 3,
+        titulo: "Close to Downtown and airport Paris house",
+        ciudad: "Reno, Nevada ",
+        parrafo: "7 Guests | 2 Bedrooms | 5 Beds | 1 Bathrooms ",
+        descripcion: "This two-bedroom, one-bathroom apartment is perfect for relaxing after exploring everything Reno has to offer. Its central location provides easy access to major attractions, restaurants, shops, and the convention center, all just minutes away. With great proximity to the airport and they will enjoy a unique experience!",
+        mapa: "pb=!1m18!1m12!1m3!1d52189.88832538525!2d-119.84190396159696!3d39.496573427775544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809940ae9292a09d%3A0x40c5c5ce7438f787!2sReno%2C%20Nevada%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1731439648490!5m2!1ses!2smx",
+        imagenes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37],
+        puntuacion: 5.0,
+        calificaciones: 1,
+        estrellas: [100, 0, 0, 0, 0],
+        link: "https://www.airbnb.mx/rooms/1256650421470537961?viralityEntryPoint=1&s=76",
+        invitados: 7,
+        entretenimiento: "For your entertainment, the property offers a TV, high quality Wifi and the large shared garden with outdoor furniture are true treasure for families with children and babies.",
+        areaFamiliar: " The Reno-Tahoe International Airport is just a five-minute drive away, making arrivals and departures extremely convenient for travelers. Additionally, the Reno-Sparks Convention Center is less than two miles away, making our property an ideal choice for those attending conferences and events. The area is surrounded by a variety of restaurants to suit all tastes. If you enjoy shopping, 'Meadowood Mall' is just a short drive away, offering a wide range of stores, from local boutiques to well-known brands. You’ll also find grocery stores and convenience shops within a short distance to cover all your needs during your stay. 'Raley's' is just a few minutes away, ideal for getting groceries and essentials. The property also has easy access to public transportation routes, making it convenient to get around the city without a car.Altogether, the location of this apartment combines the tranquility of a well - connected area with proximity to Reno's main attractions and amenities, making your stay here as comfortable as it is unforgettable."
        
         
     }
